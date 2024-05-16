a5c1e4099906d177cf6c81d253b45d6011c68ee9
billhollow <40575441+billhollow@users.noreply.github.com>
2024-05-15 22:51:13 -0500

    update token return in auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,7 +12,7 @@ export class AuthController {
     @Post('login')
     @UseGuards(LocalGuard)
     login(@Req() req: Request) {
-        return req.user;
+        return {"token" : req.user};
     }
 
     @Get('status')
